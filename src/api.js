09284e394c25ad21a5394a380d7062a8d679b723
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-06-10 15:18:30 -0600

    done

---
+++ src/api.js
@@ -0,0 +1,2 @@
+export const API_KEY = 'dNB0KaKh3y7jNTgha5EFmuUgymxKWLck817VhG1f'
+export const API_URL = 'https://api.nasa.gov/planetary/apod'
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Title.css"
+
+function Title(){
+
+
+  return (
+    <header className="title-nav">
+      <h1>NASA Photo of The Day</h1>
+    </header>
+  )
+}
+
+export default Title;
