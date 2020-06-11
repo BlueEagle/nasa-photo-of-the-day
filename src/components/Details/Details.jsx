f8e6ea68bda5102f8dd12a26e14401ce5248ea58
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-06-11 13:25:38 -0600

    wip

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,21 +1,42 @@
 import React from "react";
+import styled from 'styled-components'
 import Photo from "../Photo/Photo";
-import "./Details.css"
+// import "./Details.css"
+
+
+const DetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid gray;
+  border-radius: 20px;
+  background-color: lightgray;
+  margin: 0 3%;
+  padding: 3%;
+`
+const Explanation = styled.p`
+  width: 85%;
+`
 
 function Details(props){
   const { nasaData } = props
   const { copyright, date, explanation, title } = nasaData
 
   return (
-    <div className="details">
-      <div className="container">
+    <DetailsContainer>
+      <Container>
         <Photo nasaData={nasaData}/>
         <h1>{title}</h1>
         <h4>Date: {date}</h4>
-        <p className="explanation">{explanation}</p>
+        <Explanation>{explanation}</Explanation>
         <h6 className="copyright">{copyright}</h6>
-      </div>
-    </div>
+      </Container>
+    </DetailsContainer>
   )
 }
 
